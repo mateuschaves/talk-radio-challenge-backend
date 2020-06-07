375cd3dd2c83916ec65835760f16a537a0b0c11c
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-07 14:28:12 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ class App {
 
     middlewares() {
         this.server.use(express.json());
+        this.server.use(cors());
     }
 
 
